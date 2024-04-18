2f81bf0a0e3539042cf321b86d869655b8d84c6d
BaronYan <1229598328@qq.com>
2024-04-18 22:44:39 +0800

    字符串格式化

---
+++ test/test.4.5.2.py
@@ -0,0 +1,11 @@
+num1 = 10
+num2 = 3.141592653589793
+
+
+print(num1 + num2)
+
+print('hello %f %f' % (num1, num2))
+
+print('hello %2.0f %.2f' % (num1, num2))
+
+print('hello %2d %2d' % (num1, num2))
