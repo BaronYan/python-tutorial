1b5f9ad9e89883f3e3e66b7cd0d1e0625d2d7e7e
BaronYan <1229598328@qq.com>
2024-04-19 22:43:36 +0800

    格式化表达式

--- test/test4.6.py
+++ test/test4.6.py
@@ -1,9 +1,9 @@
 name = '公司名' # 公司名
-stock_price = 100 # 股票价格
+stock_price = 19.99 # 股票价格
 stock_code = '000001' # 股票代码
-stock_price_daily_growth_factor = 0.01 # 日增长率
-growth_days = 10 # 增长天数
+stock_price_daily_growth_factor = 1.2 # 日增长率
+growth_days = 7 # 增长天数
 
-message = f"经过 {growth_days} 后，股价将达到{(stock_price * (1 + stock_price_daily_growth_factor) ** growth_days):.2f}元。"
+finally_stock_price = f"经过 {growth_days} 后，股价将达到{(stock_price * (1 + stock_price_daily_growth_factor) ** growth_days):.2f}元。"
 
-print(message)
+print(finally_stock_price)
