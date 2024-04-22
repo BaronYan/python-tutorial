7b7108fdf09b75ba1ac3f5790522d8fb16c7639b
BaronYan <1229598328@qq.com>
2024-04-22 23:01:47 +0800

    文件的操作

--- test/test.py
+++ test/test.py
@@ -1,3 +1,2 @@
-my_dict = { "王力宏":{"age":26,"gender":"male"}, "李小璐":{"age":25,"gender":"female"}, "周杰伦": { "singer": "周杰伦", "album": "叶惠美", "year": 2004, "genre": "Pop" }}
-
-print(my_dict)
+with open('test.txt', 'w') as f:
+    f.write('Hello, world!')
